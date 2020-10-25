e8307523141f409a530e8b70f31b0fa5c7b5f2ed
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-10-25 13:48:10 +0100

    src: add example of thirdparty module usage
    This commit adds a import of a thirdparty module to show how they are
    used and that the s2i build step will resolve and download this modules
    and then perform the check/bundle.

--- example-app/src/welcome.ts
+++ example-app/src/welcome.ts
@@ -1,2 +1,3 @@
-// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-console.log("Welcome to Deno 🦕");
+import { getHours, getMinutes } from "https://deno.land/x/date_fns@v2.15.0/index.js";
+const date = new Date();
+console.log(`Welcome to Deno(${getHours(date)}:${getMinutes(date)}) 🦕`);
